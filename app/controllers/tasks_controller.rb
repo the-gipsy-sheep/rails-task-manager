e629fa8edefc7328e00cd1ea5e4270286607b538
Demian Yovanovich <subwaydemiany@gmail.com>
2022-09-10 16:18:54 -0300

    refactoring done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,15 @@
 class TasksController < ApplicationController
+  before_action :task_find, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
-
   end
 
   def create
@@ -18,17 +18,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -37,5 +34,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def task_find
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
-  patch '/tasks/:id', to: 'tasks#update'
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
+  resources :tasks
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # patch '/tasks/:id', to: 'tasks#update'
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
 end
